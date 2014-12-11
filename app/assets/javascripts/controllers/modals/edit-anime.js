f0a0a10246b74ad23ae5185d98870edf0b08f140
Thomas McNiven <hello@vevix.net>
2014-12-10 20:07:58 -0600

    fix timezone issue with dates

--- app/assets/javascripts/controllers/modals/edit-anime.js
+++ app/assets/javascripts/controllers/modals/edit-anime.js
@@ -3,19 +3,29 @@ HB.ModalsEditAnimeController = Ember.ObjectController.extend(HB.ModalControllerM
     if (v !== undefined) {
       var date = new Date(v);
       if (!isNaN(date.getTime()) && /^[0-9]{4}-[0-9]{2}-[0-9]{2}/.test(v)) {
-        this.set('startedAiring', date);
+        this.set('startedAiring', moment(v).toDate());
       }
     }
-    return moment(this.get('startedAiring')).format('YYYY-MM-DD');
+    var fmt = moment(this.get('startedAiring'));
+    if (isNaN(fmt.toDate().getTime())) {
+      return "";
+    } else {
+      return fmt.format('YYYY-MM-DD');
+    }
   }),
 
   finishedDate: Em.computed('finishedAiring', function(k, v) {
     if (v !== undefined) {
       var date = new Date(v);
       if (!isNaN(date.getTime()) && /^[0-9]{4}-[0-9]{2}-[0-9]{2}/.test(v)) {
-        this.set('finishedAiring', date);
+        this.set('finishedAiring', moment(v).toDate());
       }
     }
-    return moment(this.get('finishedAiring')).format('YYYY-MM-DD');
+    var fmt = moment(this.get('finishedAiring'));
+    if (isNaN(fmt.toDate().getTime())) {
+      return "";
+    } else {
+      return fmt.format('YYYY-MM-DD');
+    }
   })
 });
--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,3 +1,17 @@
 HB.ApplicationStore = DS.Store.extend({});
 HB.ApplicationAdapter = DS.ActiveModelAdapter.extend({});
 HB.ApplicationSerializer = DS.ActiveModelSerializer.extend({});
+
+// The internal API serializes dates as 'YYYY-MM-DD', so it only makes sense
+// to push the same format upstream.
+// Usage:
+//    DS.attr('isodate')
+HB.IsodateTransform = DS.Transform.extend({
+  serialize: function(value) {
+    return moment(value).format('YYYY-MM-DD');
+  },
+
+  deserialize: function(value) {
+    return new DS.DateTransform().deserialize(value);
+  }
+});
