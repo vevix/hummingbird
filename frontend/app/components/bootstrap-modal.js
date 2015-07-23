6b375aa3659a157d84a64e49eaa77ba3ba949f99
Thomas McNiven <hello@vevix.net>
2015-07-23 17:11:07 +1000

    fix template errors

--- frontend/app/components/bootstrap-modal.js
+++ frontend/app/components/bootstrap-modal.js
@@ -4,6 +4,10 @@ export default Ember.Component.extend({
   actions: {
     close: function() {
       return this.sendAction();
+    },
+
+    ignore: function() {
+      return false;
     }
   }
 });
